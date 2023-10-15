5e873725c46137edf686f5764c3dfab490cdb25b
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2023-10-15 22:06:19 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["El Vecino", "Mi perrro", "Mi gato", "Mi hermana", "Un ladron"];
+let action = ["destrozo", "quemo", "se comio", "me quito", "mojo"];
+let when = [
+  "ayer",
+  "anoche",
+  "al llegar a casa",
+  "esta mañana",
+  "cuando estaba en el GYM"
+];
+
+function Generadordeexcusas(quien, que, cuando) {
+  let numQuien = Math.floor(Math.random() * quien.lenght);
+  let numQue = Math.floor(Math.random() * que.lenght);
+  let numCuando = Math.floor(Math.random() * cuando.lenght);
+  return quien[numQuien] + " " + que[numQue] + " mi tarea " + cuando[numCuando];
+}
+
+window.onload = function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = Generadordeexcusas(who, action, when);
 };
